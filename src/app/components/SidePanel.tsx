c20e520d2b3240682e6205a37d79c3d0679255f4
negeek7 <negeek7@gmail.com>
2025-01-17 13:44:36 +0530

    Update SidePanel component: replace SidePanelTabs with icon imports from @heroicons/react, enhance tab options, and improve icon rendering

--- src/app/components/SidePanel.tsx
+++ src/app/components/SidePanel.tsx
@@ -1,44 +1,53 @@
 import React from 'react';
 import Icon from './Icon';
+import { HomeIcon, ChatBubbleBottomCenterIcon, UserIcon, UserGroupIcon, DocumentTextIcon, BellAlertIcon, DocumentIcon, Cog6ToothIcon } from '@heroicons/react/16/solid';
 
 
 const tabsOptions = [
     {
         value: 'Dashboard',
         iconSrc: 'home.svg',
-        iconAlt: "Dashboard Icon"
+        icon: HomeIcon,
+        iconAlt: "Dashboard Icon",
     },
     {
         value: 'Chats',
         iconSrc: 'chats.svg',
+        icon: ChatBubbleBottomCenterIcon,
         iconAlt: "Icon"
     },
     {
         value: 'Groups',
         iconSrc: 'users.svg',
+        icon: UserGroupIcon,
         iconAlt: "Icon"
     },
     {
         value: 'Contacts',
         iconSrc: 'contacts.svg',
+        icon: DocumentTextIcon,
         iconAlt: "Icon"
     },
     {
         value: 'Logs',
         iconSrc: 'bell.svg',
+        icon: BellAlertIcon,
         iconAlt: "Icon"
     },
     {
         value: 'Files',
         iconSrc: 'file.svg',
+        icon: DocumentIcon,
         iconAlt: "Icon"
     },
     {
         value: 'Settings',
         iconSrc: 'settings.svg',
+        icon: Cog6ToothIcon,
         iconAlt: "Icon"
     }
 ]
+
 
 export default function SidePanel() {
 
@@ -56,7 +65,9 @@ export default function SidePanel() {
                 {
                     tabsOptions.map((tab, index) => (
                         <div key={index} className="flex flex-row gap-4 items-center border-transparent p-2 group cursor-pointer hover:bg-gray-200 rounded-lg">
-                            <Icon iconSrc={tab.iconSrc} width={14} height={14} alt={tab.iconAlt || "Icon"} iconClass="group-hover:fill-blue-500" />
+                            {
+                                tab.icon && <tab.icon style={{ width: 18, height: 18 }} className="group-hover:text-green-600" />
+                            }
                             <p className="group-hover:text-green-600">{tab.value}</p>
                         </div>
                     ))
